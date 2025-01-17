642e707aa0a0981f015524d5b10eda28d56bb2dd
Raducu-PJ <radu.rosu@student.upt.ro>
2025-01-17 04:50:46 +0200

    Add files via upload

---
+++ src/Lab6/Main.java
@@ -0,0 +1,147 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class Main {
+    public static void scriere(List<Angajat> lista) {
+        try {
+            ObjectMapper mapper=new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            File file=new File("C:\ss\Lab6\src\angajati.json");
+            mapper.writeValue(file,lista);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static List<Angajat> citire() {
+        try {
+            File file=new File("C:\ss\Lab6\src\angajati.json");
+            ObjectMapper mapper=new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            List<Angajat> persoane = mapper
+                    .readValue(file, new TypeReference<List<Angajat>>(){});
+            return persoane;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        List<Angajat>a=new ArrayList<>();
+        a.add(new Angajat("Mircea","Cleaner", LocalDate.parse("2023-01-01"),2000));
+        a.add(new Angajat("Maria","Secretara", LocalDate.parse("2015-02-02"),1500));
+        a.add(new Angajat("Iancu","Bodyguard", LocalDate.parse("2018-03-03"),3000));
+        a.add(new Angajat("Feker","Director", LocalDate.parse("2019-04-04"),5000));
+        a.add(new Angajat("Cosmin","Barman", LocalDate.parse("2020-05-05"),4000));
+        a.add(new Angajat("Vernicescu","Hypeman", LocalDate.parse("2021-06-06"),2500));
+
+        scriere(a);
+
+        List<Angajat> Angajati=citire();
+        // Angajati.forEach(System.out::println);
+
+        Scanner sc=new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("\n");
+            System.out.println("0.Iesire");
+            System.out.println("1.Afisare angajati");
+            System.out.println("2.Afisare peste 2500");
+            System.out.println("3.Lista angajati anul trecut aprilie (sef sau director)");
+            System.out.println("4.Afisare angajati !(sef sau director)");
+            System.out.println("5.Extragerea din lista de angajați a unei liste de String-uri cu numele angajaților");
+            System.out.println("6.Afisarea salariilor <3000 (doar salariile");
+            System.out.println("7.Afisarea primului angajat din firma");
+            System.out.println("8.Salariul min/max/mediu");
+            System.out.println("9.Afisare daca exista cel putin un Ion in firma");
+            System.out.println("10.Nr Persoane angajata vara anului precedent");
+            System.out.println("Optiunea dvs:");
+            int opt=sc.nextInt();
+            switch (opt) {
+                case 0:
+                    System.exit(0);
+                case 1:
+                    Angajati.forEach(System.out::println);
+                    break;
+
+                case 2:
+                    Angajati.stream()
+                            .filter(p -> p.getSalariul() > 2500)
+                            .forEach(System.out::println);
+                    break;
+
+                case 3:
+                    List<Angajat> l = Angajati.stream()
+                            .filter(p -> p.getData_angajarii().getYear() == LocalDate.now().getYear() - 1 && p.getData_angajarii().getMonthValue() == 4 && (p.getPostul().equals("Sef") || p.getPostul().equals("Director")))
+                            .collect(Collectors.toList());
+                    l.forEach(System.out::println);
+                    break;
+
+                case 4:
+                    Angajati.stream()
+                            .filter(p -> !(p.getPostul().equals("Sef") || p.getPostul().equals("Director")))
+                            .forEach(System.out::println);
+                    break;
+                case 5:
+                    List<String> nume = Angajati.stream()
+                            .map(Angajat::getNumele)
+                            .collect(Collectors.toList());
+                    nume.forEach(System.out::println);
+                    break;
+                case 6:
+                    Angajati.stream()
+                            .filter(p -> p.getSalariul() < 3000)
+                            .map(Angajat::getSalariul)
+                            .forEach(System.out::println);
+                    break;
+                case 7:
+                    Angajati.stream()
+                            .min(Comparator.comparing(Angajat::getData_angajarii))
+                            .ifPresentOrElse(System.out::println, () -> System.out.println("Nu exista angajati"));
+                    break;
+                case 8:
+                    System.out.println("Salariul minim: "+Angajati.stream()
+                            .map(Angajat::getSalariul)
+                            .min(Comparator.comparing(c->c)).get());
+
+                    System.out.println("Salariul maxim: "+Angajati.stream()
+                            .map(Angajat::getSalariul)
+                            .max(Comparator.comparing(c->c)).get());
+
+                    System.out.println("Salariul mediu: "+Angajati.stream()
+                            .mapToDouble(Angajat::getSalariul)
+                            .sum()/Angajati.size());
+
+                    break;
+                case 9:
+                    if(Angajati.stream().anyMatch(p->p.getNumele().equals("Mircea")))
+                        System.out.println("Firma are cel puțin un Mircea angajat");
+                    else
+                        System.out.println("Firma nu are nici un Mircea angajat");
+                    break;
+                case 10:
+                    System.out.println("Numarul de angajati angajati vara anului precedent: "+Angajati.stream()
+                            .filter(p->p.getData_angajarii().getYear()==LocalDate.now().getYear()-1 && (p.getData_angajarii().getMonthValue()>=6 && p.getData_angajarii().getMonthValue()<=8))
+                            .count());
+                    break;
+                default:
+                    System.out.println("Optiune invalida");
+                    break;
+            }
+        }
+    }
+}}
